ec163d8c7876f4b5705edfa3201c416b3c8e004e
Jamie Stevens <ste616@gmail.com>
2014-11-18 19:58:28 +1100

    Change the way the results are displayed on the source resolver page and start dealing with the CSS.

--- public_html/tools/sourceResolver/sourceResolver.js
+++ public_html/tools/sourceResolver/sourceResolver.js
@@ -1,46 +1,38 @@
 // This is the Javascript required to operate sourceResolver.html.
 require( [ "astws/sourceResolver", "dojo/dom", "dojo/dom-construct", "dojo/dom-attr",
-	   "dojo/on", "dojo/keys" ],
-	 function(sourceResolver, dom, domConstruct, domAttr, on, keys) {
+	   "dojo/on", "dojo/keys", "dojo/dom-class" ],
+	 function(sourceResolver, dom, domConstruct, domAttr, on, keys, domClass) {
 
-	     // Configure the resolving module.
-	     sourceResolver.setConfig({
-		 'resolverScript': '/~ste616/cgi-bin/sourceResolver.pl'
-	     });
+	   // Configure the resolving module.
+	   sourceResolver.setConfig({
+	     'resolverScript': '/~ste616/cgi-bin/sourceResolver.pl'
+	   });
 
-	     // Do the name resolution when we someone puts a name in
-	     // the input box.
-	     on(dom.byId('source-name'), 'keydown', function(e) {
-		 if (e.keyCode === keys.ENTER) {
-		     // Resolve the name.
-		     var s = domAttr.get('source-name', 'value');
-		     sourceResolver.resolveByName(s).
-			 then(function(res) {
-			     if (res) {
-				 var ra = domConstruct.create('p', {
-				     'id': 'rightAscension',
-				     'innerHTML': 'RA: ' + res['position']['ra']
-				 });
-				 dom.byId('results-area').appendChild(ra);
-				 var dec = domConstruct.create('p', {
-				     'id': 'declination',
-				     'innerHTML': 'Dec: ' + res['position']['dec']
-				 });
-				 dom.byId('results-area').appendChild(dec);
-				 var epoch = domConstruct.create('p', {
-				     'id': 'rightAscension',
-				     'innerHTML': 'Epoch: ' + res['position']['epoch']
-				 });
-				 dom.byId('results-area').appendChild(epoch);
-			     } else {
-				 var e = domConstruct.create('p', {
-				     'id': 'errorMessage',
-				     'innerHTML': 'Could not resolve name ' + s
-				 });
-				 dom.byId('results-area').appendChild(e);
-			     }
-			 });
-		 }
-	     });
-
+	   // Do the name resolution when we someone puts a name in
+	   // the input box.
+	   on(dom.byId('source-name'), 'keydown', function(e) {
+	     if (e.keyCode === keys.ENTER) {
+	       // Resolve the name.
+	       var s = domAttr.get('source-name', 'value');
+	       sourceResolver.resolveByName(s).
+		 then(function(res) {
+		   if (res) {
+		     domAttr.set('position-right-ascension', 'innerHTML',
+				 res['position']['ra']);
+		     domAttr.set('position-declination', 'innerHTML',
+				 res['position']['dec']);
+		     domAttr.set('position-epoch', 'innerHTML',
+				 res['position']['epoch']);
+		     domClass.add('resolve-error', 'hidden');
+		     domClass.remove('position-info', 'hidden');
+		   } else {
+		     domAttr.set('resolve-error', 'innerHTML',
+				 'Could not resolve name ' + s);
+		     domClass.remove('resolve-error', 'hidden');
+		     domClass.add('position-info', 'hidden');
+		   }
+		 });
+	     }
+	   });
+	   
 	 });
