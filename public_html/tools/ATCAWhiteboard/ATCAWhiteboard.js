c7a31eb7e31be25733e8174aba2472d8f65dc4a7
Jamie Stevens <ste616@gmail.com>
2014-11-19 15:49:48 +1100

    Debugging whiteboard JS.

--- public_html/tools/ATCAWhiteboard/ATCAWhiteboard.js
+++ public_html/tools/ATCAWhiteboard/ATCAWhiteboard.js
@@ -9,10 +9,11 @@ require( [ "dojo/dom", "dojo/query", "dojo/dom-attr", "dojo/dom-class", "dojo/do
 		 var tr = dom.byId("ca0" + i + "_row");
 		 var j = 0;
 		 query("td", tr).forEach(function(node) {
-		     domClass.add(node, "space" + j++);
+		     domClass.add(node, "space" + j);
 		     if (j === 5 || j === 10 || j === 11) {
-g			 domClass.add(node, "breaktime");
+			 domClass.add(node, "breaktime");
 		     }
+		     j++;
 		 });
 		     
 	     }
