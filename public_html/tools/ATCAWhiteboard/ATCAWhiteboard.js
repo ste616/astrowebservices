86f00bfaa3c2d2b9307db48d4ab4688a326f7178
Jamie Stevens <ste616@gmail.com>
2014-11-24 07:43:02 +1100

    Restyling tables for full width and re-enabled event handler.

--- public_html/tools/ATCAWhiteboard/ATCAWhiteboard.js
+++ public_html/tools/ATCAWhiteboard/ATCAWhiteboard.js
@@ -2,30 +2,40 @@ require( [ "dojo/dom", "dojo/query", "dojo/dom-attr", "dojo/dom-class", "dojo/do
 	   "dojo/on",
 	   "dojo/NodeList-dom" ],
 	 function(dom, query, domAttr, domClass, domConstruct, on) {
+	   
+	   // Add some classes to the cells of the table.
+	   var i;
+	   for (i = 1; i <= 6; i++) {
+	     var tr = dom.byId("ca0" + i + "-row-under");
+	     var j = 0;
+	     query("td", tr).forEach(function(node) {
+	       domClass.add(node, "space" + j);
+	       if (j === 5 || j === 10 || j === 11) {
+		 domClass.add(node, "breaktime");
+	       }
+	       j++;
+	     });
 	     
-	     // Add some classes to the cells of the table.
-	     var i;
-	     for (i = 1; i <= 6; i++) {
-		 var tr = dom.byId("ca0" + i + "_row");
-		 var j = 0;
-		 query("td", tr).forEach(function(node) {
-		     domClass.add(node, "space" + j);
-		     if (j === 5 || j === 10 || j === 11) {
-			 domClass.add(node, "breaktime");
-		     }
-		     j++;
-		 });
-		     
+	   }
+	   
+	   // Highlight an area.
+	   var startBox = null;
+	   var stopBox = null;
+	   var highlightArea = function() {
+	     if (startBox === null || stopBox === null) {
+	       return;
 	     }
+	     
+	   };
 
-	     // Make some event responders.
-	     on(dom.byId('arrayTable'), 'mousedown', function(e) {
-		 console.log('mouse button pressed');
-		 console.log(e.target);
-	     });
-
-	     on(dom.byId('arrayTable'), 'mouseup', function(e) {
-		 console.log('mouse button released');
-		 console.log(e.target);
-	     });
+	   // Make some event responders.
+	   on(dom.byId('array-table-over'), 'mousedown', function(e) {
+	     console.log('mouse button pressed');
+	     console.log(e.target);
+	   });
+	   
+	   on(dom.byId('array-table-over'), 'mouseup', function(e) {
+	     console.log('mouse button released');
+	     console.log(e.target);
+	   });
 	 });
