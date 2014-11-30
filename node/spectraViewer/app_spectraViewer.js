c752d590693549b4c2295dfa139b332ef9b583a1
Jamie Stevens <ste616@gmail.com>
2014-11-30 17:36:46 +1100

    Fixing up mistakes from express.

--- node/spectraViewer/app_spectraViewer.js
+++ node/spectraViewer/app_spectraViewer.js
@@ -1,24 +1,57 @@
 // This is the node.js server for AstroWebServices.com.
 
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
+var app = require('http').createServer(handler);
+var url = require('url');
 var fs = require('fs');
-var io = require('socket.io')(server);
+var io = require('socket.io')(app);
 
-server.listen(8001);
+app.listen(8001);
 
-var displayDataset = function(dataset) {
-  io.on('connection', function(socket) {
-    socket.emit('dataset', { 'dataset': dataset });
-  });
-  
+function handler (request, response) {
+  console.log('Connection.');
+  var path = url.parse(request.url).pathname;
+
+  if (/^\/spectraViewer\//.test(path)) {
+    // User wants the spectra viewer.
+    var dr = /^\/spectraViewer\/(.*)\/*$/.exec(path);
+    var dataset = dr[1];
+    io.on('connection', function(socket) {
+      console.log('emitting dataset');
+      socket.emit('dataset', { 'dataset': dr[1] } );
+    });
+    if (typeof dataset !== 'undefined') {
+      fs.readFile('./spectraViewer.html', function(error, data) {
+	if (error) {
+	  response.writeHead(404);
+	  response.write('Unable to load spectra viewer.');
+	  response.end();
+	} else {
+	  response.writeHead(200, { 'Content-Type': 'text/html' });
+	  response.write(data, "utf8");
+	  response.end();
+	}
+      });
+    }
+  } else if (/^\/scripts\//.test(path)) {
+    // Return the user a JS.
+    var dr = /^\/scripts\/(.*)$/.exec(path);
+    console.log('requested a script ' + dr[1]);
+    fs.readFile(__dirname + "/" + dr[1], function(error, data) {
+      if (error) {
+	response.writeHead(404);
+	response.write('Unable to find script.');
+	response.end();
+      } else {
+	response.writeHead(200, { 'Content-Type': 'application/javascript' });
+	response.write(data, "utf8");
+	response.end();
+      }
+    });
+  } else {
+    // Show the default page.
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('hello world');
+    response.end();
+  }
+
 };
-
-app.param('dataset', /(.*)\/*$/);
-
-app.get('/spectraViewer/:dataset', function(req, res, next) {
-  res.sendfile(__dirname + '/spectraViewer/spectraViewer.html');
-  displayDataset(req.params.dataset);
-});
-
