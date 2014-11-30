8aaec9722d17b97cec5f5f7746a529595f4025a3
Jamie Stevens <ste616@gmail.com>
2014-11-30 17:06:26 +1100

    Pretty much gut the supplied bullshit with express

--- node/spectraViewer/app.js
+++ node/spectraViewer/app.js
@@ -3,34 +3,25 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var fs = require('fs');
+var io = require('socket.io')(server);
 
-var app = module.exports = express.createServer();
+server.listen(8001);
 
-// Configuration
+var displayDataset = function(dataset) {
+  io.on('connection', function(socket) {
+    socket.emit('dataset', { 'dataset': dataset });
+  });
+  
+};
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.param('dataset', /(.*)\/*$/);
+
+app.get('/spectraViewer/:dataset', function(req, res, next) {
+  res.sendfile(__dirname + '/spectraViewer/spectraViewer.html');
+  displayDataset(req.params.dataset);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
