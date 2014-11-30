5068206bd6ab028315c8058396d9b71d76b51d3b
Jamie Stevens <ste616@gmail.com>
2014-11-30 15:42:28 +1100

    Debugging spectra viewer along with the node server.

--- node/spectraViewer/spectraViewer.js
+++ node/spectraViewer/spectraViewer.js
@@ -1,5 +1,9 @@
-var socket = io('http://astrowebservices.com:8001');
-
-socket.on('dataset', function(data) {
-  console.log(data['dataset']);
+require( [ "dojo/dom-attr" ],
+	 function(domAttr) {
+	   var socket = io('http://astrowebservices.com:8001');
+	   
+	   socket.on('dataset', function(data) {
+	     domAttr.set('dataset-name', 'innerHTML', data['dataset']);
+	   });
 });
+
