64c907db81fff6ee97715ff2b80cd2f41d19faf5
Jamie Stevens <ste616@gmail.com>
2014-11-30 13:53:07 +1100

    Debugging the web server for node.js.

--- node/node_astrowebservices.js
+++ node/node_astrowebservices.js
@@ -9,9 +9,22 @@ var server = http.createServer(function(request, response) {
   console.log(request.url);
   var path = url.parse(request.url).pathname;
 
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write('hello world');
-  response.end();
+
+  if (/^\/spectraViewer\//.test(path)) {
+    // User wants the spectra viewer.
+    var dr = /^\/spectraViewer\/(.*)$/.exec(path);
+    var dataset = dr[1];
+    if (typeof dataset !== 'undefined') {
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write('viewing dataset ' + dataset);
+      response.end();
+    }
+  } else {
+    // Show the default page.
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('hello world');
+    response.end();
+  }
 
 });
 
