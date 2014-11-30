f0f6bd93cb75ac6620e0b47f07dc0d9caa6fe965
Jamie Stevens <ste616@gmail.com>
2014-11-30 21:30:46 +1100

    Working on determining image mouse position.

--- node/spectraViewer/spectraViewer.js
+++ node/spectraViewer/spectraViewer.js
@@ -24,17 +24,17 @@ require( [ "dojo/dom-attr", "dojo/on", "dojo/dom-geometry", "dojo/dom" ],
 	   on(dom.byId('dataset-image'), 'mousemove', function(e) {
 	     if (imgPos === null) {
 	       imgPos = domGeom.position(e.target);
-	       console.log(imgPos);
+	       // console.log(imgPos);
 	       imageTrans['top-left'] = [ imageTrans['display-x'][0],
-					  (imgPos['height'] - imageTrans['display-y'][1]) ];
+					  (imgPos['h'] - imageTrans['display-y'][1]) ];
 	       imageTrans['bottom-right'] = [ imageTrans['display-x'][1],
-					      (imgPos['height'] - imageTrans['display-y'][0]) ];
+					      (imgPos['h'] - imageTrans['display-y'][0]) ];
 	       imageTrans['display-width'] = imageTrans['display-x'][1] - imageTrans['display-x'][0];
 	       imageTrans['display-height'] = imageTrans['display-y'][1] - imageTrans['display-y'][0];	       
 	     }
 	     var xoff = e.offsetX - imageTrans['top-left'][0];
 	     var yoff = e.offsetY - imageTrans['top-left'][1];
-	     console.log(xoff + ' ' + yoff);
+	     // console.log(xoff + ' ' + yoff);
 	     if (xoff >= 0 && xoff <= imageTrans['display-width'] &&
 		 yoff >= 0 && yoff <= imageTrans['display-height']) {
 	       var px = xoff * (imageTrans['display-width'] / imageTrans['real'][0]);
